081e1a52014ca72f5c7677a200e9d9aaf829f36e
Andrew Hannebrink <andrewhannebrink@gmail.com>
2015-11-03 13:40:46 -0800

    Delete config1.js

--- config1.js
+++
@@ -1,7 +0,0 @@
-  module.exports = {
-    consumer_key: 'TH4K5gKxaAdZYGwKs6fDEgeMn',
-    consumer_secret: 'WxvxCHEEbybSxEIqFKDoOwV7SNtBY1h7nwR9CQE3wS2BwjOv0T',
-    access_token: '3236346061-SRKCm2y0awcID2NkK5ereD4CXhY6RCUaAar1q82',
-    access_token_secret: 'bVBARibhYyIejOmklzc36ieWglT00ozxqf0dSuHU6XkXG'
-  };
-
